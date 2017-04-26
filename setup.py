6679bc9b653a9788a6414d10806693c470bcb85e
c0fec0de <c0fec0de@gmail.com>
2017-04-26 21:15:29 +0200

    Fix setup.py.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ config = {
         'Programming Language :: Python :: 3.6',
     ],
     'keywords': 'defaultlist list defaultdict collections',
-    'packages': ['defaultlist'],
+    'packages': ['defaultlist.py'],
     'extras_require': {
         'dev': ['check-manifest'],
         'test': ['coverage'],
